9c54145f97e2f0f7594bf22b5a03048c6a9730f7
Mayank Suchade <mayank.suchde.4500@gmail.com>
2022-07-24 13:22:47 -0400

    Window Size context added

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import Skills from './Skills/Skills';
 import About from './About/About';
 import Background from './Background';
 import './App.scss';
-
+import {WindowContextProvider} from './context/WindowSize';
 function App() {
   return (
     <div className="App">
-      <div className='main'>
-          <Home /> 
-          <About />
-          <Skills />
-          <Projects />
-          <Contact />
-      </div>
-      <div className="bg">
-        {/* {responsiveBg} */}
-        <Background />
-      </div>      
+      <WindowContextProvider>
+        <div className='main'>
+            <Home /> 
+            <About />
+            <Skills />
+            <Projects />
+            <Contact />
+        </div>
+        <div className="bg">
+          {/* {responsiveBg} */}
+          <Background />
+        </div>      
+      </WindowContextProvider>
     </div>
   );
 }
---
+++ src/context/WindowSize.js
@@ -0,0 +1,34 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+export const WindowContext=React.createContext({width:0,height:0});
+export const WindowContextProvider=({children})=>{
+    const getVh = useCallback(() => {
+        return Math.max(
+          document.documentElement.clientHeight || 0,
+          window.innerHeight || 0
+        );
+     }, []);
+     const getVw = useCallback(() => {
+        return Math.max(
+          document.documentElement.clientWidth || 0,
+          window.innerWidth || 0
+        );
+     }, []);
+     const [clientHeight, setVh] = useState(getVh());
+     const [clientWidth, setVw] = useState(getVw());
+     useEffect(() => {
+        const handleResize = () => {
+          setVh(getVh());
+          setVw(getVw());
+        }
+        window.addEventListener('resize', handleResize);
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+     }, [getVh, getVw]);
+     return (
+        <WindowContext.Provider value={{ clientHeight, clientWidth, }} >
+        {children}
+        </WindowContext.Provider>
+        );
+}
