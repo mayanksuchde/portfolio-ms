6cbbb69ca50fc416f2a8208273280995ef8fbf3b
Mayank Suchade <mayank.suchde.4500@gmail.com>
2022-07-23 23:58:23 -0400

    background sizing bug fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
